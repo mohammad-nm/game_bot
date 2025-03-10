0fe9c89f7ad087379ac6e570f580181fbad96eaa
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2025-03-10 17:15:10 +0330

    install and confige redis

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { RedisModule } from '@nestjs-modules/ioredis';
+
+@Module({
+  imports: [
+    RedisModule.forRoot({
+      type: 'single',
+      url: 'redis://localhost:6379',
+    }),
+  ],
+})
+export class AppRedisModule {}
